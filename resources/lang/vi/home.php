2eab90cbbde9635e0a1707572247fb00cc688b25
lephuhung <lephuhung77@gmail.com>
2018-10-16 15:34:37 +0700

    Initial commit

---
+++ resources/lang/vi/home.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Home Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in home page.
+    |
+    */
+    'home' => 'Trang chủ',
+    'welcome_message' => 'Chào mừng :name,',
+    'total_sell' => 'Total Sales',
+    'total_purchase' => 'Total purchase',
+    'invoice_due' => 'Invoice due',
+    'purchase_due' => 'Purchase due',
+    'today' => 'Hôm nay',
+    'this_week' => 'Tuần này',
+    'this_month' => 'Tháng này',
+    'this_fy' => 'Năm nay',
+    'sells_last_30_days' => '30 ngày gần đây',
+    'sells_current_fy' => 'Sales Current Financial Year',
+    'total_sells' => 'Total Sales (:currency)',
+    'product_stock_alert' => 'Product Stock Alert',
+    'payment_dues' => 'Payment Dues',
+    'due_amount' => 'Due Amount',
+    'stock_expiry_alert' => 'Stock Expiry Alert',
+    'todays_profit' => "Today's profit",
+    ];
