84bfb4dc4f505948e11cfad05488d09034e0b998
lephuhung <lephuhung77@gmail.com>
2018-10-31 14:31:42 +0700

    cập nhật ngôn ngữ tiếp

--- resources/lang/vi/unit.php
+++ resources/lang/vi/unit.php
@@ -11,16 +11,16 @@ return [
     |
     */
 
-    'units' => 'Units',
-    'manage_your_units' => 'Manage your units',
-    'all_your_units' => 'All your units',
-    'name' => 'Name',
-    'short_name' => 'Short name',
-    'allow_decimal' => 'Allow decimal',
-    'added_success' => 'Unit added successfully',
-    'updated_success' => 'Unit updated successfully',
-    'deleted_success' => 'Unit deleted successfully',
-    'add_unit' => 'Add Unit',
-    'edit_unit' => 'Edit Unit',
+    'units' => 'Đơn vị',
+    'manage_your_units' => 'Quản lý đơn vị',
+    'all_your_units' => 'Tất cả đơn vị',
+    'name' => 'Tên',
+    'short_name' => 'Tên rút gọn',
+    'allow_decimal' => 'Cho phép số thập phân',
+    'added_success' => 'Đơn vị được thêm thành công',
+    'updated_success' => 'Đơn vị được cập nhật thành công',
+    'deleted_success' => 'Đơn vị được xóa thành công',
+    'add_unit' => 'Thêm đơn vị',
+    'edit_unit' => 'Sửa đơn vị',
 
 ];
